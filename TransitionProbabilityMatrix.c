62c9b2dc1545eda1f858fc2c742a9de2a540e5dc
zhuravlik <2rs.kei@gmail.com>
2015-12-16 15:00:56 +0900

    エラーメッセージ追加

--- TransitionProbabilityMatrix.c
+++ TransitionProbabilityMatrix.c
@@ -2,22 +2,31 @@
 
 int main(){
   int i,j,k,l=0;
+  double sum;
   double imputMatrix[4][4];
   double startValue[4] = {1,0,0,0};
   double outputValue[4];
 
   printf("Please imput 4*4 transition probability matrix\n");
-  
+
   for(i=0;i<4;i++){
-    printf("please imput row No.%d\n",i+1);
-    scanf("%lf %lf %lf %lf",&imputMatrix[i][0],&imputMatrix[i][1],&imputMatrix[i][2],&imputMatrix[i][3]);
+    do{
+      printf("please imput row No.%d\n",i+1);
+      scanf("%lf %lf %lf %lf",&imputMatrix[i][0],&imputMatrix[i][1],&imputMatrix[i][2],&imputMatrix[i][3]);
+      sum = imputMatrix[i][0]+imputMatrix[i][1]+imputMatrix[i][2]+imputMatrix[i][3];
+      if(sum != 1) printf("Error row's sum is not 1. Please try again\n");
+    }while(sum != 1);
   }
+
   printf("max calculation times >");
   scanf("%d",&k);
 
   printf("your imput is\n");
   for(i=0;i<4;i++){
-    printf("%f %f %f %f\n",imputMatrix[i][0],imputMatrix[i][1],imputMatrix[i][2],imputMatrix[i][3]);
+    for(j=0;j<4;j++){
+      printf("%f ",imputMatrix[i][j]);
+    }
+    printf("\n");
   }
 
 
