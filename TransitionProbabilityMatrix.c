426abdd5f2f1c04d40ef21c154ab7bd53676081f
zhuravlik <2rs.kei@gmail.com>
2015-12-17 12:54:15 +0900

    確立遷移行列の次数の可変化

--- TransitionProbabilityMatrix.c
+++ TransitionProbabilityMatrix.c
@@ -4,48 +4,78 @@
 
 int main(){
 
-  int i,j,k=0;
-  double sum;
-  double imputMatrix[4][4];
-  double startValue[4] = {1,0,0,0};
-  double outputValue[4];
+  int n,i,j,k=0;
+  int cheakRowsSum;
 
-  printf("Transition Pribability Matrix 4*4\n");
-  for(i=0;i<4;i++){
-    printf("please imput row No.%d\n",i+1);
-    scanf("%lf %lf %lf %lf",&imputMatrix[i][0],&imputMatrix[i][1],&imputMatrix[i][2],&imputMatrix[i][3]);
-    sum = imputMatrix[i][0]+imputMatrix[i][1]+imputMatrix[i][2]+imputMatrix[i][3];
-  }
 
-  printf("your imput is\n");
-  for(i=0;i<4;i++){
-    for(j=0;j<4;j++){
-      printf("%f ",imputMatrix[i][j]);
+  printf("Please imput Transition Pribability Matrix n*n (n>=2): n>");
+  scanf("%d",&n);
+
+  if(n>=2){
+    double imputMatrix[n][n];
+    double startValue[n];
+    double outputValue[n];
+    double rowsSum[n];
+
+    startValue[0] = 1;
+    for(i=1;i<n;i++){
+      startValue[i] = 0;
     }
-    printf("\n");
-  }
 
-  for(k=0;k<MaxCalcCount;k++){
-    for(i=0;i<4;i++){
-      for(j=0;j<4;j++){
-        outputValue[i] += startValue[j] * imputMatrix[j][i];
+    printf("Transition Pribability Matrix %d*%d\n",n,n);
+    for(i=0;i<n;i++){
+      for(j=0;j<n;j++){
+        printf("Prease imput Matrix element (%d,%d) >",i+1,j+1);
+        scanf("%lf",&imputMatrix[i][j]);
+        rowsSum[i] += imputMatrix[i][j];
       }
     }
 
-    for(i=0;(startValue[i] - outputValue[i])*StdPrecision < 1 && i<4 ;i++){}
-    if(i == 4) break;
+    printf("\nyour imput is\n");
+    for(i=0;i<n;i++){
+      for(j=0;j<n;j++){
+        printf("%f ",imputMatrix[i][j]);
+      }
+      printf("\n");
+    }
+    printf("\n");
 
-    for(i=0;i<4;i++){
-      startValue[i] = outputValue[i];
-      outputValue[i] = 0;
+    for(k=0;k<MaxCalcCount;k++){
+      for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+          outputValue[i] += startValue[j] * imputMatrix[j][i];
+        }
+      }
+      printf("%d times outputValue is {",k+1);
+      for(i=0;i<n;i++){
+        printf("%f",outputValue[i]);
+        if(i != n-1) printf(",");
+      }
+      printf("}\n");
+      for(i=0;(startValue[i] - outputValue[i])*StdPrecision < 1 && i<n ;i++){}
+      if(i == n) break;
+      for(i=0;i<n;i++){
+        startValue[i] = outputValue[i];
+        outputValue[i] = 0;
+      }
     }
 
-  }
+    if(MaxCalcCount != k){
+      printf("\noutputValue is {");
+      for(i=0;i<n;i++){
+        printf("%f",outputValue[i]);
+        if(i != n-1) printf(",");
+      }
+      printf("}\n");
 
-  if(MaxCalcCount != k){
-    printf("outputValue is {%f,%f,%f,%f}\n",outputValue[0],outputValue[1],outputValue[2],outputValue[3]);
-    printf("calculation time == %d\n",k+1);
+      printf("calculation time == %d\n",k+1);
+      return 0;
+    }else{
+      printf("Error Over max calculation times. Prease cheak your imput matrix\n");
+      return 1;
+    }
   }else{
-    printf("Error Over max calculation times. Prease cheak your imput matrix or max calculation times\n");
+    printf("Error! n must be 2 or more!");
+    return 1;
   }
 }
